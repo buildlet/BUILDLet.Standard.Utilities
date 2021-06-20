35f07bd8cb3ae9bfccf79198184944d961642a59
dsakam <dsakam@live.jp>
2021-06-20 20:29:24 +0900

    commit for v1.6.0 (FC)

---
+++ BUILDLet.Standard.Utilities/MagicPacket.cs
@@ -0,0 +1,223 @@
+﻿/***************************************************************************************************
+The MIT License (MIT)
+
+Copyright 2015 Daiki Sakamoto
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+associated documentation files (the "Software"), to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or 
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT 
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+***************************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+using BUILDLet.Standard.Utilities.Properties;
+using BUILDLet.Standard.Diagnostics;
+
+namespace BUILDLet.Standard.Utilities.Network
+{
+    /// <summary>
+    /// マジックパケット (AMD Magic Packet Format) を実装します。
+    /// </summary>
+    public static class MagicPacket
+    {
+        // ----------------------------------------------------------------------------------------------------
+        // Constructor(s)
+        // ----------------------------------------------------------------------------------------------------
+        // (None)
+
+
+        // ----------------------------------------------------------------------------------------------------
+        // Static Properties
+        // ----------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// 許容されている MAC アドレスの既定の区切り文字を取得します。
+        /// </summary>
+        public static readonly char[] Delimiters = { ':', '-' };
+
+
+        // ----------------------------------------------------------------------------------------------------
+        // Static Method(s)
+        // ----------------------------------------------------------------------------------------------------
+
+        // Get Datagram as Byte Array
+        private static byte[] GetDatagram(string macAddress)
+        {
+#if DEBUG
+            Debug.WriteLine($"MAC Address=\"{macAddress}\"", DebugInfo.ShortName);
+#endif
+
+            // Validation
+            bool match = false;
+            foreach (var separator in MagicPacket.Delimiters)
+            {
+                if (Regex.IsMatch($"{macAddress}{separator}", $"^((([0-9A-Za-z]{{2}}){separator}){{6}})$")) { match = true; }
+            }
+            if (!match)
+            {
+                // ERROR
+                throw new FormatException(Resources.InvalidArgumentErrorMessage);
+            }
+
+
+            // for UDP Datagram (Byte Array)
+            byte[] dgram = new byte[6 * (1 + 16)];
+
+            // SET Header (0xFF * 6)
+            (new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF }).CopyTo(dgram, 0);
+
+            // Convert MAC Address from String to Byte Array
+            var mac = macAddress.Split(MagicPacket.Delimiters).ToList().ConvertAll(hex => Convert.ToByte(hex, 16));
+
+            // SET MAC Address * 16
+            for (int i = 0; i < 16; i++) { mac.ToArray().CopyTo(dgram, 6 + (i * 6)); }
+
+#if DEBUG
+            for (int i = 0; i < (1 + 16); i++)
+            {
+                Debug.Write($"Magic Packet[{i:D2}]=0x", DebugInfo.ShortName);
+                for (int j = 0; j < 6; j++) { Debug.Write($"{dgram[(i * 6) + j]:X2}"); }
+                Debug.WriteLine("");
+            }
+#endif
+
+            // RETURN
+            return dgram;
+        }
+
+
+        // Send Datagram
+        private static async Task<string[]> SendDatagramAsync(string macAddress, int port, int count, int interval, bool async)
+        {
+            // GET UDP Datagram (Byte Array)
+            var dgram = MagicPacket.GetDatagram(macAddress);
+
+            // List of IP Address sent
+            var sent = new List<string>();
+
+
+            // for NetworkInterface(s)
+            foreach (var address in Dns.GetHostAddresses(Dns.GetHostName()))
+            {
+                // only for IPv4
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    var lep = new IPEndPoint(address, 0);
+                    var udp = new UdpClient(lep);
+                    var ep = new IPEndPoint(IPAddress.Broadcast, port);
+
+                    for (int i = 0; i < count; i++)
+                    {
+                        // Send Datagram (Async or Sync)
+                        var bytes = async ? await udp.SendAsync(dgram, dgram.Length, ep) : udp.Send(dgram, dgram.Length, ep);
+
+                        if (bytes > 0)
+                        {
+#if DEBUG
+                            Debug.Write($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}] Send Magic Packet{(async ? " [Async]" : "")} (", DebugInfo.ShortName);
+                            Debug.Write($"{i + 1}: MAC Address=\"{macAddress}\", {bytes} Bytes");
+                            Debug.WriteLine($", Destination Port={port}, Source IP Address={lep.Address})");
+#endif
+
+                            if (i < count - 1)
+                            {
+                                // Wait for Interval
+                                if (async)
+                                {
+                                    // Async
+                                    await Task.Delay(interval);
+                                }
+                                else
+                                {
+                                    // NOT Async
+                                    Thread.Sleep(interval);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            // ERROR
+                            throw new InvalidOperationException();
+                        }
+                    }
+
+                    // Add IP Address sent
+                    sent.Add(address.ToString());
+                }
+            }
+
+
+            // RETURN
+            return sent.ToArray();
+        }
+
+
+        /// <summary>
+        /// マジックパケット (AMD Magic Packet Format) を送信します。
+        /// </summary>
+        /// <param name="macAddress">
+        /// MAC アドレスの文字列を指定します。
+        /// </param>
+        /// <param name="port">
+        /// リモートマシンのポート番号を指定します。
+        /// 既定のポート番号は <c>2304</c> 番です。
+        /// </param>
+        /// <param name="count">
+        /// マジックパケットを送信する回数を指定します。
+        /// 既定の回数は <c>1</c> 回です。
+        /// </param>
+        /// <param name="interval">
+        /// マジックパケットを送信する間隔を、ミリ秒単位で指定します。
+        /// 既定の回数は <c>0</c> ミリ秒です。
+        /// </param>
+        /// <returns>
+        /// マジックパケットを送信した IP アドレスの文字列配列を返します。
+        /// </returns>
+        public static string[] Send(string macAddress, int port = 2304, int count = 1, int interval = 0)
+            => MagicPacket.SendDatagramAsync(macAddress, port, count, interval, false).Result;
+
+
+        /// <summary>
+        /// マジックパケット (AMD Magic Packet Format) を送信します。
+        /// </summary>
+        /// <param name="macAddress">
+        /// MAC アドレスの文字列を指定します。
+        /// </param>
+        /// <param name="port">
+        /// リモートマシンのポート番号を指定します。
+        /// 既定のポート番号は <c>2304</c> 番です。
+        /// </param>
+        /// <param name="count">
+        /// マジックパケットを送信する回数を指定します。
+        /// 既定の回数は <c>1</c> 回です。
+        /// </param>
+        /// <param name="interval">
+        /// マジックパケットを送信する間隔を、ミリ秒単位で指定します。
+        /// 既定の回数は <c>0</c> ミリ秒です。
+        /// </param>
+        /// <returns>
+        /// マジックパケットを送信した IP アドレスの文字列配列を返します。
+        /// </returns>
+        public static Task<string[]> SendAsync(string macAddress, int port = 2304, int count = 1, int interval = 0)
+            => MagicPacket.SendDatagramAsync(macAddress, port, count, interval, true);
+    }
+}
---
+++ BUILDLet.Standard.UtilitiesTests/MagicPacketTests.cs
@@ -0,0 +1,233 @@
+﻿/***************************************************************************************************
+The MIT License (MIT)
+
+Copyright 2021 Daiki Sakamoto
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+associated documentation files (the "Software"), to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or 
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT 
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+***************************************************************************************************/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Net.NetworkInformation;  // for NetworkInterface
+
+using BUILDLet.Standard.Utilities.Network;
+using BUILDLet.Standard.Diagnostics;
+
+namespace BUILDLet.Standard.Utilities.Network.Tests
+{
+    [TestClass]
+    public class MagicPacketTests
+    {
+        // Get Number of NetworkInterfaces of Local Machine (Excluding Loopback Address)
+        private static int GetNumberOfNetworkInterfaces() =>
+            NetworkInterface.GetAllNetworkInterfaces().Where(ni => ni.OperationalStatus == OperationalStatus.Up).Count() - 1;
+
+
+        [TestMethod]
+        [TestCategory("Manual")]
+        public void WolTest() => MagicPacket.Send("E4:11:5B:AD:32:38", count: 3, interval: 1000);
+
+
+        [DataTestMethod]
+        [TestCategory("Exception")]
+        // [ExpectedException(typeof(FormatException))]
+        [ExpectedException(typeof(AggregateException))]
+        [DataRow("00+00-00-00-00-00")]
+        [DataRow("00+00+00+00+00+00")]
+        [DataRow("00-00-00-00-00-0-")]
+        public void SendMacAddressInvalidFormatTest(string macAddress)
+        {
+            // ACT & ASSERT
+            MagicPacket.Send(macAddress);
+        }
+
+
+        [DataTestMethod]
+        [TestCategory("Exception")]
+        [ExpectedException(typeof(FormatException))]
+        // [ExpectedException(typeof(AggregateException))]
+        [DataRow("00+00-00-00-00-00")]
+        [DataRow("00+00+00+00+00+00")]
+        [DataRow("00-00-00-00-00-0-")]
+        public async Task SendAsyncMacAddressInvalidFormatTest(string macAddress)
+        {
+            try
+            {
+                // ACT & ASSERT
+                await MagicPacket.SendAsync(macAddress);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+        [DataTestMethod]
+        [DataRow("00-00-00-FF-FF-FF", -1, -1, -1, DisplayName = "w/o Parameter")]
+        [DataRow("FF:FF:FF:00:00:00", 9, 5, 0)]
+        [DataRow("01:23:45:AB:CD:EF", 9, 3, 100)]
+        [DataRow("AB:CD:EF:01:23:45", 9, 2, 1000)]
+        public void SendTest(string macAddress, int port, int count, int interval)
+        {
+            // ARRANGE
+            var expected = MagicPacketTests.GetNumberOfNetworkInterfaces();
+
+            // ACT: Send Magic Packet
+            var sent = (port < 0 || count < 0 || interval < 0) ?
+                MagicPacket.Send(macAddress) :
+                MagicPacket.Send(macAddress, port, count, interval);
+
+            // OUTPUT
+            for (int i = 0; i < sent.Length; i++)
+            {
+                Debug.WriteLine($"Source IP Address[{i}] = {sent[i]}");
+            }
+
+            // ASSERT
+            Assert.AreEqual(expected, sent.Length);
+        }
+
+
+        [DataTestMethod]
+        [DataRow(false, DisplayName = "NOT Async")]
+        [DataRow(true, DisplayName = "Async")]
+        public void SendSyncTest(bool async)
+        {
+            // ARRANGE
+            var this_MethodName = $"{nameof(MagicPacketTests)}.{nameof(MagicPacketTests.SendSyncTest)}";
+            var act_MethodName = async ?
+                $"{nameof(MagicPacketTests)}.{nameof(MagicPacketTests.SendSyncTestAsync)}" :
+                $"{nameof(MagicPacket)}.{nameof(MagicPacket.Send)}";
+
+            var macAddress = "01:23:45:AB:CD:EF";
+            var port = 9;
+            var count = 2;
+            var interval = 1000;
+            var error = 100;
+
+            var expected_count = MagicPacketTests.GetNumberOfNetworkInterfaces();
+            var expected_timespan = TimeSpan.FromMilliseconds(interval * (count - 1) * MagicPacketTests.GetNumberOfNetworkInterfaces());
+
+            string[] sent = null;
+            Task<string[]> task = null;
+
+
+            // OUTPUT: START
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] {this_MethodName}: START");
+
+            // OUTPUT: Before Call
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] {this_MethodName}() --> {act_MethodName}()");
+            Console.WriteLine();
+
+
+            // GET Start Time
+            var start_time = DateTime.Now;
+
+            // ACT
+            if (!async)
+            {
+                // NOT Async
+                sent = MagicPacket.Send(macAddress, port, count, interval);
+            }
+            else
+            {
+                // Async
+                task = async ? SendSyncTestAsync(macAddress, port, count, interval) : null;
+            }
+
+            // GET Timespan
+            var time = DateTime.Now - start_time;
+
+
+            // OUTPUT: Returned
+            Console.WriteLine();
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] {this_MethodName}() <-- {act_MethodName}()");
+
+
+            // Wait for complete (Async)
+            if (async)
+            {
+                // OUTPUT: Before Wait for Complete
+                Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] {this_MethodName}: Wait for Complete {act_MethodName}()");
+                Console.WriteLine("-----");
+                Console.WriteLine();
+
+                // Wait for complete (Async)
+                task.Wait();
+
+                // OUTPUT: After Wait for Complete
+                Console.WriteLine();
+                Console.WriteLine("-----");
+                Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] {this_MethodName}: {act_MethodName}() is completed.");
+            }
+
+            
+            // OUTPUT: END (before Assertion)
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] {this_MethodName}: END (Count = {expected_count}, Timespan = {time.TotalMilliseconds})");
+
+
+            // ASSERT (Count)
+            Assert.AreEqual(expected_count, !async ? sent.Length : task.Result.Length);
+
+            // ASSERT (Timespan)
+            if (!async)
+            {
+                // NOT Async
+                Assert.IsTrue((time > (expected_timespan - TimeSpan.FromMilliseconds(error))) && (time < (expected_timespan + TimeSpan.FromMilliseconds(error))));
+            }
+            else
+            {
+                // Async
+                Assert.IsTrue(time < TimeSpan.FromMilliseconds(error));
+            }
+        }
+
+        // SendSyncTestAsync
+        public static async Task<string[]> SendSyncTestAsync(string macAddress, int port, int count, int interval)
+        {
+            // ARRANGE
+            var this_MethodName = $"{nameof(MagicPacketTests)}.{nameof(MagicPacketTests.SendSyncTestAsync)}";
+            var act_MethodName = $"{nameof(MagicPacket)}.{nameof(MagicPacket.SendAsync)}";
+
+
+            // OUTPUT: START
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] {this_MethodName}: START");
+
+            // OUTPUT: Before Call
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] {this_MethodName}() --> {act_MethodName}()");
+
+
+            // ACT
+            var task = await MagicPacket.SendAsync(macAddress, port, count, interval);
+
+
+            // OUTPUT: Returned
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] {this_MethodName}() <-- {act_MethodName}()");
+
+            // OUTPUT: END
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] {this_MethodName}: END");
+
+
+            // RETURN
+            return task;
+        }
+    }
+}
